c7fa25315c373db840446d5af45ee3bc0570357e
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-19 13:32:39 +0000

    Création de la bibliothèque SimpleFunctionsLib

---
+++ CrudUtilityLib/ExtensionsClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudUtilityLib
+{
+    public static class ExtensionsClass
+    {
+        public static string Afficher(this DateTime? date)
+        {
+            return date.HasValue ? date.Value.ToString("dd-MM-yyyy") : "Non Défini";
+        }
+
+        public static string AfficherTemps(this DateTime? date)
+        {
+            return date.HasValue ? date.Value.ToString("dd-MM-yyyy HH:mm") : "Non Défini";
+        }
+
+        public static string Afficher(this double? nombre)
+        {
+            if (nombre.HasValue)
+                return nombre.Value.ToString("### ### ### ### ##0.###");
+            else return "Non Défini";
+        }
+
+        public static string Afficher(this decimal? nombre)
+        {
+            if (nombre.HasValue)
+                return nombre.Value.ToString("### ### ### ### ##0.###");
+            else return "Non Défini";
+        }
+    }
+}
