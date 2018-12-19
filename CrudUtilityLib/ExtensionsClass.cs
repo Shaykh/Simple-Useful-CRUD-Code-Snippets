fbf280072eeae6ed016ec1d2c0b3cb61c3702edb
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-19 15:07:59 +0000

    Ajout des docstring de la fonction Afficher

--- CrudUtilityLib/ExtensionsClass.cs
+++ CrudUtilityLib/ExtensionsClass.cs
@@ -8,23 +8,40 @@ namespace CrudUtilityLib
 {
     public static class ExtensionsClass
     {
+        /// <summary>
+        /// Extension function to display date object at dd-MM-yyyy format
+        /// </summary>
+        /// <param name="date">Nullable DateTime object</param>
+        /// <returns>Formated date or Non Défini</returns>
         public static string Afficher(this DateTime? date)
         {
             return date.HasValue ? date.Value.ToString("dd-MM-yyyy") : "Non Défini";
         }
-
+        /// <summary>
+        /// Extension function to display date object at dd-MM-yyyy HH:mm format
+        /// </summary>
+        /// <param name="date">Nullable DateTime object</param>
+        /// <returns>Formated date or Non Défini</returns>
         public static string AfficherTemps(this DateTime? date)
         {
             return date.HasValue ? date.Value.ToString("dd-MM-yyyy HH:mm") : "Non Défini";
         }
-
+        /// <summary>
+        /// Extension function to display double number to french number format
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <returns>formated number or Non Défini</returns>
         public static string Afficher(this double? nombre)
         {
             if (nombre.HasValue)
                 return nombre.Value.ToString("### ### ### ### ##0.###");
             else return "Non Défini";
         }
-
+        /// <summary>
+        /// Extension function to display decimal number to french number format
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <returns>formated number or Non Défini</returns>
         public static string Afficher(this decimal? nombre)
         {
             if (nombre.HasValue)
